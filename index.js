f31c927ab3e639cd4cc5b99251d976d90689c186
SaranrajManureva <31609305+SaranrajManureva@users.noreply.github.com>
2024-01-25 12:33:29 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello, this is your Node.js server running on port 2500!\n');
+});
+
+const PORT = 2500;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}/`);
+});
